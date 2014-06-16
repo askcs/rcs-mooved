ca6c93f849accb3f11b97b91d4d7423af1d00925
lvdinten <lvdinten@ask-cs.com>
2014-06-16 13:42:45 +0200

    init update

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
                     middleware: function(connect) {
                         return [
                             connect.static('.tmp'),
-                            connect().use('/bower_components', connect.static('./bower_components')),
+                            connect().use('/app/bower_components', connect.static('./app/bower_components')),
                             connect.static(config.app)
                         ];
                     }
